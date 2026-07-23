72d54721fffaa60a17eefe19f3540252907db992
Adam <78690375+adamchgour@users.noreply.github.com>
2026-07-23 10:18:03 +0200

    Add: evaluate-reverse-polish-notation - submission-2

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-2.py
@@ -0,0 +1,20 @@
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        num = []
+
+        for token in tokens :
+            if token not in "+-*/":
+                num.append(int(token))
+            else:
+                b = num.pop()
+                a = num.pop()
+
+                if token == '+':
+                    num.append(a+b)
+                elif token == '-':
+                    num.append(a-b)
+                elif token == '*':
+                    num.append(a*b)
+                elif token == '/':
+                    num.append(int(a/b))
+        return num[-1]
