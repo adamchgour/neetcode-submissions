8d25f9fc54dee8be2cf7faacfcffc9fa7ff7b502
Adam <78690375+adamchgour@users.noreply.github.com>
2026-09-13 19:44:39 +0200

    Add: minimum-window-with-characters - submission-1

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-1.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        ht = {} 
+        hs = {}
+        have = 0
+        l,r = 0,0
+        mini = [0,len(s),False]
+
+        for elt in t:
+            if elt in ht:
+                ht[elt] += 1
+            else :
+                ht[elt] = 1
+
+        need = len(ht)
+
+        while r < len(s) or have == need:
+
+            if need != have:
+                if s[r] in ht :
+                    if s[r] in hs:
+                        hs[s[r]] += 1
+                    else :
+                        hs[s[r]] = 1
+                    if  ht[s[r]] == hs[s[r]]:
+                        have += 1  
+                r += 1
+            else :
+                if s[l] in ht :
+                    if hs[s[l]] == ht[s[l]]:
+                        have -= 1
+                    hs[s[l]] -= 1
+                if (r-l <= mini[1]-mini[0]):
+                    mini = [l,r,True]
+                l += 1  
+
+        if mini[2]:
+            return s[mini[0]:mini[1]]
+        else:
+            return ""
